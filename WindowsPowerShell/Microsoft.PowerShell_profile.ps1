2de8dc2a0e891fbe0060cf852a36487621773911
jblondeau2 <jblondeau2@gmail.com>
2012-06-30 15:51:49 +0200

    PowershellConfig
    init

---
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,8 @@
+function runscc {C:\wamp\bin\php\php5.3.5\php.exe app/console cache:clear}
+function runsccdev {C:\wamp\bin\php\php5.3.5\php.exe app/console cache:clear --env=dev}
+function runsccprod {C:\wamp\bin\php\php5.3.5\php.exe app/console cache:clear --env=prod}
+function gowww {cd C:\wamp}
+Set-Alias -name scc -value runscc
+Set-Alias -name sccdev -value runsccdev
+Set-Alias -name sccprod -value runsccprod
+Set-Alias -name go-www -value gowww
