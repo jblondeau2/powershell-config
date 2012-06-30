c8bfda98d513ff16d9694a0774b905132e4593a6
jblondeau2 <jblondeau2@gmail.com>
2012-06-30 16:15:21 +0200

    Update gowww

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,7 @@
 function runscc {C:\wamp\bin\php\php5.3.5\php.exe app/console cache:clear}
 function runsccdev {C:\wamp\bin\php\php5.3.5\php.exe app/console cache:clear --env=dev}
 function runsccprod {C:\wamp\bin\php\php5.3.5\php.exe app/console cache:clear --env=prod}
-function gowww {cd C:\wamp}
+function gowww {cd C:\wamp\www}
 Set-Alias -name scc -value runscc
 Set-Alias -name sccdev -value runsccdev
 Set-Alias -name sccprod -value runsccprod
