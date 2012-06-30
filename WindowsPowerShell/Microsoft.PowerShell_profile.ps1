e48a13b81d65ee280619825ca2f4a1ccd9b5598c
jblondeau2 <jblondeau2@gmail.com>
2012-06-30 19:57:05 +0200

    Rename features

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -5,11 +5,15 @@ function gowww {cd C:\wamp\www}
 function genEntity {C:\wamp\bin\php\php5.3.5\php.exe app/console doctrine:generate:entity}
 function genBundle {C:\wamp\bin\php\php5.3.5\php.exe app/console generate:bundle}
 function routerDebug {C:\wamp\bin\php\php5.3.5\php.exe app/console router:debug}
+function schemaUpdate {C:\wamp\bin\php\php5.3.5\php.exe app/console doctrine:schema:update}
+function schemaCreate {C:\wamp\bin\php\php5.3.5\php.exe app/console doctrine:schema:create}
 
 Set-Alias -name scc -value runScc
 Set-Alias -name scc-dev -value runSccDev
 Set-Alias -name scc-prod -value runSccProd
 Set-Alias -name go-www -value goWww
-Set-Alias -name generate-entity -value genEntity
-Set-Alias -name generate-bundle -value genBundle
-Set-Alias -name router-debug -value routerDebug
+Set-Alias -name genE -value genEntity
+Set-Alias -name genB -value genBundle
+Set-Alias -name routD -value routerDebug
+Set-Alias -name schU -value schemaUpdate
+Set-Alias -name schC -value schemaCreate
